21ad529a0152223846386290ea828b620742e52d
damyan olierook <themesk11@live.nl>
2019-01-25 02:42:51 +0100

    Commit heeft geupdate folder structure, efficiency update. betere backend en alvast klaar voor frontend

---
+++ html/login.php
@@ -0,0 +1,26 @@
+<html>
+    <meta charset="utf-8">    
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="../assets/include/style.css">
+    <? include($_SERVER['DOCUMENT_ROOT']."/Apotheek/CONSTANTS.php"); ?>
+    <?php 
+    include_once dirname(__DIR__).'/bin/php/handlers/dbh.php'; //DB HANDLER
+
+    ?>
+
+    
+    <body>
+    <div class="header">
+
+    
+    </div>
+    <form style="login">
+
+        <input type="text" id="fname" name="fname">
+    </form>
+        
+    <h1><?php //connect(); ?></h1>
+
+    </body>
+
+</html>
