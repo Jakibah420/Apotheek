980fb5a0c6f153fdc8812bda6ec33ea0b7c6eb38
blamudia <themesk11@live.nl>
2019-01-30 17:53:04 +0100

    Small changes to login / porting to desktop with main push

--- html/login.php
+++ html/login.php
@@ -8,6 +8,7 @@
     include_once dirname(__DIR__).'/bin/php/handlers/dbh.php'; //DB HANDLER
     ?>
 
+
     
     <body>
     <div class="loginregisterform">
@@ -15,23 +16,31 @@
         <button class="registerbutton" onclick="login(1)">registreer</button>
         <div id="loginform" class="loginform">
         <form class="login">
+        <div class="regwrap">
             <div class="label"><label class="label">Email</label></div>
             <input type="text" id="username" name="username/email">
             <div class="label"><label class="label">Wachtwoord</label></div>
             <input type="password" id="password" name="password">
+            <div class="label3"></div>
+        </div>
+            <input type="submit" />
         </form>
         </div>
         <div id="registerform" class="registerform">
+        
+        <form class="login" onsubmit="return checkForm(this);">
         <div class="regwrap">
-        <form class="login">
             <div class="label2"><label class="label">Email</label></div>
             <input class="reginput" type="text" id="username" name="username/email">
-            <div class="label2"><label class="label">Password</label></div>
+            <div class="label2"><label class="label">Wachtwoord</label></div>
             <input class="reginput" type="password" id="password" name="password">
-            <div class="label2"><label class="label">Repeat Password</label></div>
-            <input class="reginput" type="password" id="password" name="password">
-        </form>
+            <div class="label2"><label class="label">Herhaal Wachtwoord</label></div>
+            <input class="reginput" type="password" id="password2" name="password2">
+            <div class="label2"></div>
         </div>
+            <input type="submit" />
+        </form>
+        
         </div>
     </div>
 
