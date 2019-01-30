c563ed0226e6ae273f9b94ad2b58b3758c4a339c
unknown <caseykruijer@gmail.com>
2019-01-30 12:29:28 +0100

    commit

--- html/home.php
+++ html/home.php
@@ -3,27 +3,37 @@
 	<head>
 		<title>appotheek schut</title>
 		<meta charset="UTF-8">
-		<meta name="description" content="dit is de appotheek van meneer schut">
+		<meta name="description" content="dit is het appotheek van meneer schut">
 		<meta name="keywords" content="schut, appotheek, medicijnen, ">
 		<meta name="author" content="Casey Kruijer">
 		<meta name="viewport"content="width=device-witdth, initial-scale=1-0">			
 		<link rel="stylesheet" href="../bin/css/home.css">
 	</head>
+	
 	<body>
+	
 		<header class="header">
 			<img src="..\assets\img\logo.png" alt="hier staat ons logo" id="logo">
 		</header>
-			<div class="topnav">
-				<a class="active" href="#home">Home</a>
-				<a href="">Service</a>
-				<a href="">Medicijn</a>
-				<a href="">Bestel</a>
-				<a href="">Contact</a>
-				<a href="login.php">Log in</a>
-			</div>	
-			<p>appotheek staat in aalsmeerderbrug</p>
-			<div class="footer">
-				<p>Apotheek schut</p>
+		
+		<div class="topnav">
+			<a class="active" href="#home">Home</a>
+			<a href="">Service</a>
+			<a href="">Medicijn</a>
+			<a href="">Bestel</a>
+			<a href="">Contact</a>
+			<a href="login.php">Log in</a>
+		</div>	
+		
+			<p>Oosteinderweg 180||1432 AR|| Aalsmeer</p>
+			
+			<div id="google">
+				<iframe src="https://www.google.com/maps/embed?pb=!1m17!1m11!1m3!1d7113.126349423197!2d4.771230837696514!3d52.28345505288039!2m2!1f159.7880357833916!2f0!3m2!1i1024!2i768!4f35!3m3!1m2!1s0x47c5e09730634c59%3A0x3a474eaad0680ecb!2sOosteinderweg+180%2C+1432+AR+Aalsmeer!5e1!3m2!1snl!2snl!4v1548847177965" width="300" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
 			</div>
+		
+		<footer class="footer">
+			<p>apotheek schut</p>
+		
+		</footer>
 	</body>
 </html>
