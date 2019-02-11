5f13cdd73ba965c287a6aeeb91d28b2ec4490fff
Iakovos Petrou <253006@cursist.rockopnh.nl>
2019-02-11 09:31:09 +0100

    Added index redirect to home.

--- index.php
+++ index.php
@@ -2,7 +2,6 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
 <?php
-$path = $_SERVER['DOCUMENT_ROOT'];
-$path .= "/Apotheek/html/home.php";
-require_once($path);
+$path = "html/home.php";
+header('Location: '. $path);
 ?>
