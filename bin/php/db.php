7651c89e3dac96d38671a9e4207c159c0fc6f88a
damyan olierook <themesk11@live.nl>
2019-02-16 13:58:59 +0100

    Changing host -client "Damyan" small changes to setup

--- bin/php/db.php
+++ bin/php/db.php
@@ -57,5 +57,41 @@ if (isset($_POST['login_user'])) {
 			$showDivFlag=true;
 		}
 	}
-  }
+	}
+	
+	//SETUP CODE
+	$serverLink = mysqli_connect($host, $username, $password, $db);
+	$user_check = $_SESSION['username'];
+	$ses_sql = mysqli_query($serverLink, "SELECT email FROM users WHERE email = '$user_check' ");
+	$row = mysqli_fetch_array($ses_sql,MYSQLI_ASSOC);
+	$login_session = $row['email'];
+	if(!isset($_SESSION['username'])){
+		 header("location:login.php");
+		 die();
+	}
+	if (isset($_POST['setup_user'])) {
+		$email = mysqli_real_escape_string($serverLink, $_POST['regemail']);
+		$password_1 = mysqli_real_escape_string($serverLink, $_POST['regpassword']);
+		$password_2 = mysqli_real_escape_string($serverLink, $_POST['regpassword2']);
+	
+		$select = mysqli_query($serverLink, "SELECT * FROM users WHERE email = '$email'") or exit(mysqli_error($serverLink));
+		if(mysqli_num_rows($select) == 1) {
+			array_push($errors, "EmExists");
+			$showRegMailFlag=true;
+		}
+	
+		if (count($errors) == 0) {
+			$regepassword = md5($password_1);//encrypt the password before saving in the database
+	
+			$query = "INSERT INTO users (email, pass) 
+						VALUES('$email', '$regepassword')";
+			mysqli_query($serverLink, $query);
+			$_SESSION['username'] = $email;
+			header('location: setup.php '); //NAAR USER INTERFACE!
+		 }
+		else {
+			$showRegMailFlag=true;
+		}
+	}
+
 ?>
--- html/setup.php
+++ html/setup.php
@@ -5,62 +5,79 @@
     <?php 
     include($_SERVER['DOCUMENT_ROOT']."/Apotheek/CONSTANTS.php");
     require($_SERVER['DOCUMENT_ROOT']."/Apotheek/assets/include/header.php");
-    include($_SERVER['DOCUMENT_ROOT']."/Apotheek/bin/php/handlers/session.php");
+    include($_SERVER['DOCUMENT_ROOT']."/Apotheek/bin/php/db.php");
     ?>
     
+    <script>
+        function CloseForm(){
+            document.getElementById("InfoPanel").style.display="none";
+        } function Enabled(form){
+            if (document.getElementById("voorwaarden").checked == true ) {
+                return true;
+            } else {
+                document.getElementById("Labelacv").style.color = 'red';
+                alert("Om uw account the activeren moet u de voorwaarden accepteren");
+                return false;
+            }
+        }
+    </script>
 
     <body style="background: linear-gradient(rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.2)), url('/Apotheek/assets/img/login.jpg');background-repeat:no-repeat;background-size: cover;">
     <div class="setupform">
         <div id="setupform" class="setupformmin">
-        <form class="setup" method="post" action="setup.php">
+        <form class="setup" method="post" action="setup.php" onsubmit="return Enabled(this)">
             <div style="margin-left:30px;" class="setupinput">
                 <div class="label4"><label class="label4">Email</label></div>
                 <input style="margin-right:10px; width:200px;height:30px;" type="text" id="username" name="logemail" disabled value="<?php echo $login_session; ?>"></label>
             </div>
-            <div style="margin-left:10px;" class="setupinput">
+            <div style="margin-left:20px;" class="setupinput">
                 <div class="label4"><label class="label4">Naam</label></div>
-                <input style="margin-right:10px; width:150px;height:30px;" type="text" id="name" name="logemail" ></label>
+                <input style="margin-right:10px; width:150px;height:30px;" type="text" id="name" name="name" ></label>
             </div>
-            <div style="margin-left:10px;" class="setupinput">
+            <div style="margin-left:20px;" class="setupinput">
                 <div class="label4"><label class="label4">Tussenvoegsels</label></div>
                 <input style="margin-right:10px; width:150px;height:30px;" type="text" ></label>
             </div>
-            <div style="margin-left:10px;" class="setupinput">
+            <div style="margin-left:20px;" class="setupinput">
                 <div class="label4"><label class="label4">Achternaam</label></div>
-                <input style="margin-right:10px; width:150px;height:30px;" type="text" id="sirname" name="logemail" ></label>
+                <input style="margin-right:10px; width:150px;height:30px;" type="text" id="sirname" name="sirname" ></label>
             </div><br/>
             <div style="margin-left:30px;" class="setupinput">
                 <div class="label4"><label class="label4">Geboortedatum</label></div>
-                <input style="margin-right:10px; width:200px;height:30px;" type="date" id="sirname" name="logemail" ></label>
+                <input style="margin-right:10px; width:200px;height:30px;" type="date" id="birth" name="birth" ></label>
             </div>
-            <div style="margin-left:10px;" class="setupinput">
+            <div style="margin-left:20px;" class="setupinput">
                 <div class="label4"><label class="label4">IBAN</label></div>
-                <input style="margin-right:10px; width:200px;height:30px;" type="text" id="sirname" name="logemail" ></label>
+                <input style="margin-right:10px; width:200px;height:30px;" type="text" id="IBAN" name="iban" ></label>
             </div>
-            <div style="margin-left:10px;" class="setupinput">
+            <div style="margin-left:20px;" class="setupinput">
                 <div class="label4"><label class="label4">Geslacht</label></div>
-                <input style="margin-right:10px; width:20px;height:20px;" type="radio" id="gmale" name="gmale" checked vale="Man">Man</label>
-                <input style="margin-right:10px; width:20px;height:20px;" type="radio" id="gmale" name="gfemale" vale="Vrouw">Vrouw</label>
-                <input style="margin-right:10px; width:20px;height:20px;" type="radio" id="gmale" name="gother" vale="Anders">Anders</label>
+                <input style="margin-right:10px; width:20px;height:20px;" type="radio" id="gmale" name="gender" checked vale="Man">Man</label>
+                <input style="margin-right:10px; width:20px;height:20px;" type="radio" id="gmale" name="gender" vale="Vrouw">Vrouw</label>
+                <input style="margin-right:10px; width:20px;height:20px;" type="radio" id="gmale" name="gender" vale="Anders">Anders</label>
             </div><br/>
             <div style="margin-left:30px;" class="setupinput">
                 <div class="label4"><label class="label4">Mobiele Telefoon</label></div>
-                <input style="margin-right:10px; width:200px;height:30px;" type="text" id="sirname" name="logemail" ></label>
+                <input style="margin-right:10px; width:200px;height:30px;" type="text" id="mobile" name="mobilephone" ></label>
             </div>
-            <div style="margin-left:10px;" class="setupinput">
+            <div style="margin-left:20px;" class="setupinput">
                 <div class="label4"><label class="label4">Telefoon</label></div>
-                <input style="margin-right:10px; width:200px;height:30px;" type="text" id="sirname" name="logemail" ></label>
+                <input style="margin-right:10px; width:200px;height:30px;" type="text" id="number" name="phone" ></label>
             </div><br/>
             <div style="margin-left:30px;" class="setupinput">
                 <div class="label4"><label class="label4">Adres</label></div>
-                <input style="margin-right:10px; width:200px;height:30px;" type="text" id="sirname" name="logemail" ></label>
+                <input style="margin-right:10px; width:200px;height:30px;" type="text" id="adres" name="adres" ></label>
             </div>
-            <div style="margin-left:10px;" class="setupinput">
+            <div style="margin-left:20px;" class="setupinput">
                 <div class="label4"><label class="label4">Postcode</label></div>
-                <input style="margin-right:10px; width:200px;height:30px;" type="text" id="sirname" name="logemail" ></label>
+                <input style="margin-right:10px; width:200px;height:30px;" type="text" id="postal" name="postal" ></label>
             </div>
-            <input class="submit2" type="submit" name="setup_user" value="Klaar"/>
+            <input class="submit2" type="submit" id="setupdone" name="setup_user" value="Klaar"/>
+            <br/><br/><div style="margin-left:30px;" class="setupinput">
+                <input style="bottom:0; margin-right:10px; width:20;height:20px;" type="checkbox" id="voorwaarden" name="voorwaarden"><label id="Labelacv">Accepteer voorwaarden.</label>
+            </div>
         </form>
+        <div id="InfoPanel" class="InfoPanel"><button style="border-style:solid;background:darkred;border-color:darkred;left:30;position:absolute;" onclick="CloseForm()">X</button><br/><br/>Vul uw gegevens zo zorgvuldig <br/> mogenlijk in!<br/><br/> U kunt de informatie <br> na invoer nog veranderen!</div>
     </div>
 
     </body>
