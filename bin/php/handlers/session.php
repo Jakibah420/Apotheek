bbb9f6c103ea9fb6b5dbc0eb88f14116d913229c
Iakovos Petrou <iakopetrou@gmail.com>
2019-02-22 13:02:59 +0100

    Combined the sql queries to one big query.

--- bin/php/handlers/session.php
+++ bin/php/handlers/session.php
@@ -9,27 +9,22 @@
    $serverLink = mysqli_connect($host, $username, $password, $db);
    $user_check = $_SESSION['username'];
    $ses_sql = mysqli_query($serverLink, "SELECT email FROM users WHERE email = '$user_check' ");
-   $set_sql = mysqli_query($serverLink, "SELECT email FROM userInfo WHERE email = '$user_check' ");
-   $sqlquery = mysqli_query($serverLink, "SELECT * FROM userInfo WHERE email = '$user_check'");
+   $set_sql = mysqli_query($serverLink, "SELECT * FROM userInfo WHERE email = '$user_check' ");
    
    $row = mysqli_fetch_array($ses_sql,MYSQLI_ASSOC);
    $rowt = mysqli_fetch_array($set_sql,MYSQLI_ASSOC);
-   //$namt = mysqli_fetch_array($nam,MYSQLI_ASSOC);
-   //$srnamt = mysqli_fetch_array($srnam,MYSQLI_ASSOC);
-   //$gendt = mysqli_fetch_array($srgendt,MYSQLI_ASSOC);
-   $fetched =  mysqli_fetch_array($sqlquery,MYSQLI_ASSOC);
    
-   $login_session = $fetched['email'];
-   $name_session = $fetched['surname'];
-   $sname_session = $fetched['ename'];
-   $gend = $fetched['gender'];
-   $firstname = $fetched['nam'];
-   $phone = $fetched['phone'];
-   $mphone = $fetched['mobilephone'];
-   $adres = $fetched['adres'];
-   $postal = $fetched['postal'];
-   $birthdate = $fetched['birthdate'];
-   $activation = $fetched['activation_date'];
+   $login_session = $rowt['email'];
+   $name_session = $rowt['surname'];
+   $sname_session = $rowt['ename'];
+   $gend = $rowt['gender'];
+   $firstname = $rowt['nam'];
+   $phone = $rowt['phone'];
+   $mphone = $rowt['mobilephone'];
+   $adres = $rowt['adres'];
+   $postal = $rowt['postal'];
+   $birthdate = $rowt['birthdate'];
+   $activation = $rowt['activation_date'];
     
    
    if(!isset($_SESSION['username'])){
