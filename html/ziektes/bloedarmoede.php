14de66c0334744db5ebc79dbe4c79d150261e0cc
damyan olierook <themesk11@live.nl>
2019-02-25 18:28:55 +0100

    HREF FIXED

--- html/ziektes/bloedarmoede.php
+++ html/ziektes/bloedarmoede.php
@@ -8,6 +8,9 @@
 		<meta name="author" content="Jacob Ligthart">
 		<meta name="viewport"content="width=device-witdth, initial-scale=1-0">
 		<link rel="stylesheet" href="/Apotheek/bin/css/medicijnen.css">
+		<link rel="stylesheet" href="/Apotheek/bin/css/index.css">
+		<link rel="stylesheet" href="/Apotheek/bin/css/style.css">
+		<link rel="stylesheet" href="/Apotheek/bin/css/footeroverride.css">
 		<link rel="icon" type="image/png" href="/Apotheek/assets/img/MiniLogo.png">
 	</head>
 
@@ -27,7 +30,7 @@
 			<a href="../service.php">Service</a>
 			<a href="../medicatie.php">Medicijnen</a>
 			<a href="../voorlichting.php">Voorlichting</a>
-			<a href="bestel.php">Bestel</a>
+			<a href="../bestel.php">Bestel</a>
 			<a class="disabled" href="#">Contact</a>
 			<a style="float:right;margin-right:8%;" <?php if(!isset($_SESSION['username'])){echo"href='login.php'";}else{echo"href='logout.php'";}?>><?php if(!isset($_SESSION['username'])){echo"Log in / Registreer";}else{echo"Log uit";}?></a>
 		</div>
